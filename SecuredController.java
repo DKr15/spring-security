2101fbd02096321efe56a7fbfce456a3ea6913b8
Dileep <44550447+DKr15@users.noreply.github.com>
2021-08-01 15:10:56 +0530

    Add files via upload

---
+++ SecuredController.java
@@ -0,0 +1,16 @@
+package com.example.spring.security;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secured")
+public class SecuredController {
+
+    @GetMapping("")
+    public String sayHello(){
+        return "Hello sir from secured zone";
+    }
+}
